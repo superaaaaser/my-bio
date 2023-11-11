fb1dc3458c6cf6dca0e1184e7ba9c22b7776bd63
mylittleunicorn <nasir.dev48@gmail.com>
2023-11-11 13:18:01 +0700

    pertama push

---
+++ src/components/FooterComponents.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FooterComponents = () => {
+  return <div>FooterComponents</div>;
+};
+
+export default FooterComponents;
---
+++ src/components/NavbarComponents.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function NavbarComponents() {
+  return <div className="container mx-auto bg-slate-500">NavbarComponents</div>;
+}
